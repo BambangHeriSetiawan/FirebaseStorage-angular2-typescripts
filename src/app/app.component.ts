9af35cb36e75c66b7997bf94532fa9086f78e7ee
iravenomala <ira.v@students.amikom.ac.id>
2017-04-02 14:58:49 +0700

    sample

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,73 @@
-import { Component } from '@angular/core';
+import { Component,ElementRef, Input, Renderer } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
+import * as firebase from 'firebase';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  private isUploadBtn= true ;
+  public TheFileContents:any;
+   items: FirebaseListObservable<any[]>;
+constructor(
+  public af:AngularFire,
+  public el:ElementRef
+){
+  this.items = af.database.list('/items');
+}
+fileChange(event:any){
+
+let filelist :FileList = event.target.files;
+if(filelist.length>0){
+      var metadata = {
+      contentType: 'image/jpeg'
+    };
+    let file : File = filelist[0];
+    console.log(file.name);
+    var storageRef = firebase.storage().ref();
+     var uploadTask = storageRef.child('images/' + file.name).put(file, metadata);
+// Listen for state changes, errors, and completion of the upload.
+uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
+  function(snapshot) {
+    // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+    var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+    console.log('Upload is ' + progress + '% done');
+    switch (snapshot.state) {
+      case firebase.storage.TaskState.PAUSED: // or 'paused'
+        console.log('Upload is paused');
+        break;
+      case firebase.storage.TaskState.RUNNING: // or 'running'
+        console.log('Upload is running');
+        break;
+    }
+  }, function(error) {
+
+  // A full list of error codes is available at
+  // https://firebase.google.com/docs/storage/web/handle-errors
+  switch (error.message) {
+    case 'storage/unauthorized':
+      // User doesn't have permission to access the object
+      break;
+
+    case 'storage/canceled':
+      // User canceled the upload
+      break;
+
+
+    case 'storage/unknown':
+      // Unknown error occurred, inspect error.serverResponse
+      break;
+  }
+}, function() {
+  // Upload completed successfully, now we can get the download URL
+  var downloadURL = uploadTask.snapshot.downloadURL;
+  var txtDownload = <HTMLInputElement>document.getElementById('imgurl');
+  txtDownload.value = downloadURL;
+});
+  
+}
+window.location.reload();
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { AngularFireModule } from 'angularfire2';
 import { AppComponent } from './app.component';
+export const firebaseConfig = {
+apiKey: "AIzaSyBc0mNgKNFhP-flnnOT3wPw7Vvv-cMrgtg",
+    authDomain: "homesuvenir.firebaseapp.com",
+    databaseURL: "https://homesuvenir.firebaseio.com",
+    projectId: "homesuvenir",
+    storageBucket: "homesuvenir.appspot.com",
+    messagingSenderId: "940309053377"
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +20,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
