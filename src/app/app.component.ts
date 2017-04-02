e69d32affb5a2d1894fcebe927011a3ab1b6e07c
iravenomala <ira.v@students.amikom.ac.id>
2017-04-02 15:35:28 +0700

    fix html code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,7 +27,7 @@ if(filelist.length>0){
     let file : File = filelist[0];
     console.log(file.name);
     var storageRef = firebase.storage().ref();
-     var uploadTask = storageRef.child('cards/' + file.name).put(file, metadata);
+     var uploadTask = storageRef.child('images/' + file.name).put(file, metadata);
 // Listen for state changes, errors, and completion of the upload.
 uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
   function(snapshot) {
@@ -63,8 +63,7 @@ uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
 }, function() {
   // Upload completed successfully, now we can get the download URL
   var downloadURL = uploadTask.snapshot.downloadURL;
-  var txtDownload = <HTMLInputElement>document.getElementById('imgurl');
-  txtDownload.value = downloadURL;
+
 });
   
 }
